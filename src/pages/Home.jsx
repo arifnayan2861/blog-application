818750cae81b5b7b529649aca4d95ecf021ba269
arifnayan2861 <am.nayan2861@gmail.com>
2023-11-29 19:34:21 +0600

    sidebar section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
 import Banner from "../components/Banner";
+import BlogPage from "../components/BlogPage";
 
 const Home = () => {
   return (
     <div>
       <Banner />
+      <div className="max-w-7xl mx-auto">
+        <BlogPage />
+      </div>
     </div>
   );
 };
